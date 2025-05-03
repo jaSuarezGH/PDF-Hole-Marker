5e9869469d80e37f9c9f51498cffc144b2d8a641
José Suárez <74939658+jaSuarezGH@users.noreply.github.com>
2025-05-03 18:06:24 -0400

    Fixed awful logic redundancy

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pymupdf
 HOLE_MARGIN: int = 10
 
 def draw_holes_by_number(hole_number:int, doc: any, hole_margin: int = HOLE_MARGIN):
-    n_holes = hole_number + 1
 
     try:
         for page_index in range(len(doc)): # Iterate over pdf pages
@@ -12,10 +11,10 @@ def draw_holes_by_number(hole_number:int, doc: any, hole_margin: int = HOLE_MARG
             rect = page.rect # Get page rectangle (dimentions)
             
             # Draw holes
-            for x in range(n_holes-1):
+            for x in range(hole_number):
 
                 # Calculate hole Y position
-                hole_y_pos += rect.height / n_holes
+                hole_y_pos += rect.height / (hole_number + 1)
 
                 page.draw_circle((hole_margin, hole_y_pos), radius=3, color=(0, 0, 0), width=1.2)
 
@@ -23,7 +22,6 @@ def draw_holes_by_number(hole_number:int, doc: any, hole_margin: int = HOLE_MARG
         print("An exception occurred", e)
 
 def draw_holes_by_number_and_space(hole_number:int, hole_distance:int, doc: any, hole_margin: int = HOLE_MARGIN):
-    n_holes = hole_number + 1
 
     try:
         for page_index in range(len(doc)): # Iterate over pdf pages
@@ -32,7 +30,7 @@ def draw_holes_by_number_and_space(hole_number:int, hole_distance:int, doc: any,
             hole_y_pos = ((rect.height - (hole_distance)*hole_number))/2
             
             # Draw holes
-            for x in range(n_holes-1):
+            for x in range(hole_number):
 
                 page.draw_circle((hole_margin, hole_y_pos), radius=3, color=(0, 0, 0), width=1.2)
 
@@ -50,13 +48,14 @@ def set_ouput_file_name(file_name:str) -> str:
 
 if __name__ == "__main__":
 
-    file_name: str = clean_file_name(input("Enter PDF file name:"))
+    #file_name: str = clean_file_name(input("Enter PDF file name:"))
+    file_name: str = "documento.pdf"
     output_file_name: str = set_ouput_file_name(file_name)
 
     try:
         doc = pymupdf.open(file_name) # Open a document
-        draw_holes_by_number(3, doc)
-        #draw_holes_by_number_and_space(3, 141, doc)
+        #draw_holes_by_number(5, doc)
+        draw_holes_by_number_and_space(3, 141, doc)
         doc.save(output_file_name) # Save the document with a new filename
     except Exception as e:
         print("An exception occurred", e)
